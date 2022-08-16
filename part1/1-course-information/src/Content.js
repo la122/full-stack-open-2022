621dbbeb328e1ed6f4894c3cdc8287a34505b31a
la122 <70019096+la122@users.noreply.github.com>
2022-08-16 14:30:18 +0200

    exercise 1.4 done

--- part1/1-course-information/src/Content.js
+++ part1/1-course-information/src/Content.js
@@ -4,9 +4,9 @@ const Content = (props) => {
   console.log(props);
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
