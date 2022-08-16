f9ba4ce0e21370938ed5800abf760f1e2d2eeca1
la122 <70019096+la122@users.noreply.github.com>
2022-08-16 14:18:10 +0200

    exercise 1.3 done

--- part1/1-course-information/src/Content.js
+++ part1/1-course-information/src/Content.js
@@ -1,11 +1,12 @@
 import Part from "./Part";
 
 const Content = (props) => {
+  console.log(props);
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.part1} />
+      <Part part={props.part2} />
+      <Part part={props.part3} />
     </div>
   );
 };
