c6032ef4a3b83e2fcf6c9192c530cea41b64ebd3
la122 <70019096+la122@users.noreply.github.com>
2022-08-18 15:29:07 +0200

    2.5 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,33 +1,4 @@
-const Header = ({ name }) => <h1>{name}</h1>;
-
-const Total = ({ sum }) => <b>total of {sum} exercises</b>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Course = ({ course }) => {
-  const total = course.parts.reduce((soFar, part) => soFar + part.exercises, 0);
-  console.log(total);
-
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={total} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
