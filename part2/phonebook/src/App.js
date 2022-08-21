231cabcda61275e2f56ecea19dc0b8dc0166d336
la122 <70019096+la122@users.noreply.github.com>
2022-08-21 16:32:20 +0200

    2.19 and 2.20 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,34 @@
 import { useState, useEffect } from "react";
 import personService from "./services/persons";
 
+const Notification = ({ message, color }) => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setShow(true);
+    const timeout = setTimeout(() => {
+      setShow(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [message]);
+
+  if (message === null || show === false) {
+    return null;
+  }
+
+  const style = {
+    color: color,
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+
+  return <div style={style}>{message}</div>;
+};
+
 const Filter = ({ onChange }) => (
   <div>
     filter shown with <input onChange={onChange} />
@@ -47,6 +75,10 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setNewFilter] = useState("");
+  const [notification, setNotification] = useState({
+    message: null,
+    color: null,
+  });
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -55,12 +87,10 @@ const App = () => {
   }, []);
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value);
     setNewNumber(event.target.value);
   };
 
@@ -79,20 +109,33 @@ const App = () => {
       );
       if (confirmed) {
         const changedPerson = { ...personFound, number: newPerson.number };
-        console.log("updating person to", changedPerson);
-        personService.update(changedPerson).then((returnedPerson) => {
-          console.log("updated", returnedPerson);
-          setPersons(
-            persons.map((p) =>
-              p.id !== returnedPerson.id ? p : returnedPerson
-            )
-          );
-          // setPersons(persons.concat(changedPerson));
-        });
+        personService
+          .update(changedPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((p) =>
+                p.id !== returnedPerson.id ? p : returnedPerson
+              )
+            );
+            setNotification({
+              message: `Updated ${returnedPerson.name}`,
+              color: "green",
+            });
+          })
+          .catch(() => {
+            setNotification({
+              message: `Information of ${newPerson.name} has already been removed from server`,
+              color: "red",
+            });
+          });
       }
     } else {
       personService.create(newPerson).then((returnedPerson) => {
         setPersons(persons.concat(returnedPerson));
+        setNotification({
+          message: `Added ${returnedPerson.name}`,
+          color: "green",
+        });
       });
     }
   };
@@ -100,7 +143,7 @@ const App = () => {
   const deleteButtonClicked = (event) => {
     event.preventDefault();
     const id = event.target.value;
-    const person = persons.find((person) => person.id === +id); // cast id to string
+    const person = persons.find((person) => person.id === +id);
     if (window.confirm(`Delete ${person.name}?`)) {
       deletePerson(id);
     }
@@ -109,7 +152,7 @@ const App = () => {
   const deletePerson = (id) => {
     personService.remove(id).then(() => {
       const personsUpdated = persons.filter((person) => {
-        return person.id !== +id; // cast id to string
+        return person.id !== +id;
       });
       setPersons(personsUpdated);
     });
@@ -125,6 +168,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification.message} color={notification.color} />
       <Filter onChange={handleFilterChange} />
 
       <h2>add a new</h2>
