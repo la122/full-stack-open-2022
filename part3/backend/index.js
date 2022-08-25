71abe8ac1509d3fbd2a883d2405df6ca940c712c
la122 <70019096+la122@users.noreply.github.com>
2022-08-25 23:06:01 +0200

    3.13 done

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+const Person = require("./models/person");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 app.use(
   morgan("tiny", {
@@ -25,43 +26,22 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    } people.</p><p>${new Date()}</p>`
-  );
-});
+// app.get("/info", (request, response) => {
+//   response.send(
+//     `<p>Phonebook has info for ${
+//       persons.length
+//     } people.</p><p>${new Date()}</p>`
+//   );
+// });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -82,7 +62,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
