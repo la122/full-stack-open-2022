5deef67cb61efe5fd946fc944a6eb9ce8e3e9466
la122 <70019096+la122@users.noreply.github.com>
2022-09-08 14:09:43 +0200

    4.5 done

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -4,7 +4,13 @@ const dummy = () => {
 
 const totalLikes = (blogs) => blogs.reduce((sum, item) => sum + item.likes, 0);
 
+const favoriteBlog = (blogs) =>
+  blogs.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
