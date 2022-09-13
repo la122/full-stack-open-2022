a9bb45853e45ce427c1bcae01dbc04c51818907c
la122 <70019096+la122@users.noreply.github.com>
2022-09-13 14:18:09 +0200

    4.8 done

---
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+const { blogs } = require("./sample_data");
+
+const initialBlogs = blogs;
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(initialBlogs);
+});
+
+test("returns the correct amount of blog posts in JSON format", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
