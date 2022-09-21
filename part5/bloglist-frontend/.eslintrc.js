73254153a1d73b7e377347843d2406ee2ead852a
la122 <70019096+la122@users.noreply.github.com>
2022-09-21 17:42:55 +0200

    part5 setup

---
+++ part5/bloglist-frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
