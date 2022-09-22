fe6aec0771b026ff3a87b2f79a520c9c183e3688
la122 <70019096+la122@users.noreply.github.com>
2022-09-22 13:49:47 +0200

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogWithoutDetails = () => (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <Button text="view" handleClick={() => setDetailsVisible(true)} />
+    </div>
+  )
+
+  const blogWithDetails = () => (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <Button text="hide" handleClick={() => setDetailsVisible(false)} />
+      <br />
+      {blog.url}
+      <br />
+      {blog.likes} <Button text="like" />
+      <br />
+      {blog.user?.name}
+    </div>
+  )
+
+  return <>{detailsVisible ? blogWithDetails() : blogWithoutDetails()}</>
+}
 
 export default Blog
