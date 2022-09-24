38fa77478d58c5c059ea4629aa3d8ddaaae37c45
la122 <70019096+la122@users.noreply.github.com>
2022-09-24 21:55:16 +0200

    6.9 done

--- anecdotes/src/components/AnecdoteList.js
+++ anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => state.anecdotes)
   anecdotes.sort((a, b) => b.votes - a.votes)
   const dispatch = useDispatch()
 
