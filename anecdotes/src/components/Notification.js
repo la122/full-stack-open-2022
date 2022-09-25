ff2ef3b0be6f97e5d99c0c1c04167310f7e552fb
la122 <70019096+la122@users.noreply.github.com>
2022-09-26 00:56:25 +0200

    6.19 done

--- anecdotes/src/components/Notification.js
+++ anecdotes/src/components/Notification.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const [show, setShow] = useState(true)
 
   useEffect(() => {
@@ -25,4 +25,12 @@ const Notification = () => {
   return notification && <div style={style}>{notification.message}</div>
 }
 
-export default Notification
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
