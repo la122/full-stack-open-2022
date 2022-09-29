909bb4ed72525c398fdea85e86ff1e56ce765048
la122 <70019096+la122@users.noreply.github.com>
2022-09-30 00:45:32 +0200

    8.10 done

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -21,3 +21,25 @@ export const ALL_BOOKS = gql`
     }
   }
 `
+
+export const CREATE_BOOK = gql`
+  mutation AddBook(
+    $title: String!
+    $published: Int!
+    $genres: [String!]!
+    $author: String!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      genres: $genres
+      author: $author
+    ) {
+      title
+      author
+      published
+      id
+      genres
+    }
+  }
+`
