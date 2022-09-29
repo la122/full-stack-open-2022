5f33ddee8e9fe6b7253f13ad13283f82dcf53b8a
la122 <70019096+la122@users.noreply.github.com>
2022-09-29 21:35:17 +0200

    7.21 done, added space between blog cards

--- part7/blogapp/frontend/src/pages/BlogListPage.js
+++ part7/blogapp/frontend/src/pages/BlogListPage.js
@@ -3,6 +3,8 @@ import Togglable from '../components/Togglable'
 import NewBlogForm from '../components/NewBlogForm'
 import { useRef } from 'react'
 import { Heading, HeadingLevel } from 'baseui/heading'
+import { useStyletron } from 'baseui'
+import { FlexGrid, FlexGridItem } from 'baseui/flex-grid'
 
 const BlogListPage = ({ blogs }) => {
   const blogFormRef = useRef()
@@ -10,16 +12,24 @@ const BlogListPage = ({ blogs }) => {
   const createBlog = async (blog) => {
     blogFormRef.current.toggleVisibility()
   }
+
   return (
     <div>
       <HeadingLevel>
         <Heading>Blogs</Heading>
-        <Togglable buttonLabel="create new" ref={blogFormRef}>
-          <NewBlogForm onCreate={createBlog} />
-        </Togglable>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
+        <FlexGrid flexGridRowGap="scale400">
+          <FlexGridItem>
+            <Togglable buttonLabel="Create a new blog" ref={blogFormRef}>
+              <NewBlogForm onCreate={createBlog} />
+            </Togglable>
+          </FlexGridItem>
+
+          {blogs.map((blog) => (
+            <FlexGridItem key={blog.id}>
+              <Blog blog={blog} />
+            </FlexGridItem>
+          ))}
+        </FlexGrid>
       </HeadingLevel>
     </div>
   )
