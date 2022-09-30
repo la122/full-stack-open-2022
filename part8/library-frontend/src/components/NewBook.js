543d7716cde811b5d1134aa99102331297f013c0
la122 <70019096+la122@users.noreply.github.com>
2022-09-30 23:36:48 +0200

    8.22 done

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -10,7 +10,7 @@ const NewBook = (props) => {
   const [genres, setGenres] = useState([])
 
   const [createBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+    refetchQueries: [ALL_BOOKS, ALL_AUTHORS]
   })
 
   if (!props.show) {
