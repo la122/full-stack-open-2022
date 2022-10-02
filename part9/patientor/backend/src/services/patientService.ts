f5674e7f5b623b639b9d8dc6636c3360ddba53a4
la122 <70019096+la122@users.noreply.github.com>
2022-10-02 20:58:06 +0200

    9.19 done

--- part9/patientor/backend/src/services/patientService.ts
+++ part9/patientor/backend/src/services/patientService.ts
@@ -1,4 +1,4 @@
-import patientData from "../../data/patients.json";
+import patientData from "../../data/patients";
 import { Patient, PublicPatient, NewPatient } from "../types";
 import { v1 as uuid } from "uuid";
 import { parseGender } from "../utils";
--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -4,9 +4,6 @@ export interface Diagnose {
   latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
-
 export enum Gender {
   Male = "male",
   Female = "female",
@@ -23,6 +20,52 @@ export type Patient = {
   entries: Entry[];
 };
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
 export type PublicPatient = Omit<Patient, "ssn" | "entries">;
 
 export type NewPatient = Omit<Patient, "id" | "entries">;
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
